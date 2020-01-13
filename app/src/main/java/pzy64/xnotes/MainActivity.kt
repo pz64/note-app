646e2af0124150f27c2b5bcb1c5d76c653ee09d5
Prafuldev <thepzy64@gmail.com>
2020-01-13 15:04:45 +0530

    initial setup

---
+++ app/src/main/java/pzy64/xnotes/MainActivity.kt
@@ -0,0 +1,14 @@
+package pzy64.xnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pzy64.xnotes.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
---
+++ app/src/main/java/pzy64/xnotes/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pzy64.xnotes.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
