6a2f9924aaa1a9fa0694b22a1730befbe13f61e9
Prafuldev <thepzy64@gmail.com>
2020-02-03 20:02:37 +0530

    completed about screen

--- app/src/main/java/pzy64/xnotes/ui/screens/about/AboutFragment.kt
+++ app/src/main/java/pzy64/xnotes/ui/screens/about/AboutFragment.kt
@@ -1,16 +1,18 @@
 package pzy64.xnotes.ui.screens.about
 
 
-import android.os.Build
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marcoscg.licenser.LicenserDialog
 import kotlinx.android.synthetic.main.fragment_about.*
 import pzy64.xnotes.BuildConfig
-
 import pzy64.xnotes.R
+
 
 class AboutFragment : Fragment() {
 
@@ -28,9 +30,22 @@ class AboutFragment : Fragment() {
         versionNumber.text = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
 
         viewGithubRepoButton.setOnClickListener {
-
+            val url = "https://github.com/pz64/note-app"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
         }
+
+        rateButton.setOnClickListener {
+            val url = ""
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+
     }
+
+
 
 
 }
